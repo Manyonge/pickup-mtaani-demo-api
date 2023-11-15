d4bc3cbee5ff48bc592565202f37f52dde36033c
manyonge <arthurmanyonge22@gmail.com>
2023-11-15 15:17:22 +0300

    - initial open api set up successfull

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Pickup Mtaani Demo Api')
+    .setDescription('A demo api to demonstrate to pickup mtaani the api and endpoints that are required')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
